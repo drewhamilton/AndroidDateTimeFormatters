23176e9c8519a9dd9f4125b965a083c03b8bd7ef
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-06-11 22:08:31 +0200

    Delete unneeded test Activity

--- test/src/main/java/drewhamilton/androiddatetimeformatters/test/TimeSettingWithPermissionTest.java
+++ test/src/main/java/drewhamilton/androiddatetimeformatters/test/TimeSettingWithPermissionTest.java
@@ -1,5 +1,7 @@
 package drewhamilton.androiddatetimeformatters.test;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.provider.Settings;
 import androidx.test.rule.ActivityTestRule;
@@ -20,21 +22,20 @@ import static org.junit.Assert.assertTrue;
  */
 public abstract class TimeSettingWithPermissionTest extends TimeSettingTest {
 
-    @Rule public final ActivityTestRule<SystemSettingsTestActivity> testActivityRule =
-            new ActivityTestRule<>(SystemSettingsTestActivity.class);
+    @Rule public final ActivityTestRule<Activity> testActivityRule = new ActivityTestRule<>(Activity.class);
 
     @Before
     public void getWriteSettingsPermission() {
-        SystemSettingsTestActivity activity = getActivity();
+        Activity activity = getActivity();
         if (Build.VERSION.SDK_INT >= 23 && !Settings.System.canWrite(activity)) {
-            activity.requestWriteSettingsPermission();
+            activity.startActivity(new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS));
             // TODO: Implement correct navigation
             onView(withId(android.R.id.checkbox)).perform(click());
             assertTrue(Settings.System.canWrite(activity));
         }
     }
 
-    protected final SystemSettingsTestActivity getActivity() {
+    protected final Activity getActivity() {
         return testActivityRule.getActivity();
     }
 }
