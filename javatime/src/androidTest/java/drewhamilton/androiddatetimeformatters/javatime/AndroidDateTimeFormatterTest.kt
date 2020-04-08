627193cc7cb8e3dfafdd065708ae60b728dccb53
Drew Hamilton <drew.hamilton.0@gmail.com>
2020-04-08 16:18:00 +0200

    AGP 4.0.0-beta04
    Tests work on API 21

--- javatime/src/androidTest/java/drewhamilton/androiddatetimeformatters/javatime/AndroidDateTimeFormatterTest.kt
+++ javatime/src/androidTest/java/drewhamilton/androiddatetimeformatters/javatime/AndroidDateTimeFormatterTest.kt
@@ -9,7 +9,7 @@ import org.junit.Test
 import java.time.LocalTime
 import java.util.Locale
 
-@RequiresApi(26) // TODO WORKAROUND: Desugaring should work for tests too
+@RequiresApi(21) // Instrumented tests for Dynamic Features is not supported on API < 21 (AGP 4.0.0-beta04)
 class AndroidDateTimeFormatterTest : TimeSettingTest() {
 
     @Test fun ofLocalizedTime_nullSystemSettingUsLocale_uses12HourFormat() {
@@ -64,7 +64,7 @@ class AndroidDateTimeFormatterTest : TimeSettingTest() {
 
         val formatter = AndroidDateTimeFormatter.ofLocalizedTime(testContext)
         val formattedTime = formatter.format(TIME)
-        assertEquals(FORMATTED_TIME_12, formattedTime)
+        assertEquals(FORMATTED_TIME_12_ITALY, formattedTime)
     }
 
     @Test fun ofLocalizedTime_24SystemSettingItalyLocale_uses24HourFormat() {
@@ -84,6 +84,7 @@ class AndroidDateTimeFormatterTest : TimeSettingTest() {
         private val TIME = LocalTime.of(16, 44)
 
         private const val FORMATTED_TIME_12 = "4:44 PM"
+        private const val FORMATTED_TIME_12_ITALY = "04:44 PM"
         private const val FORMATTED_TIME_24 = "16:44"
     }
 }
