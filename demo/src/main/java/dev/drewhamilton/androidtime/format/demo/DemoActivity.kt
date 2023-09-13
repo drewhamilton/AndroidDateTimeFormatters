8d1642fce9b63ee7395d8f48bce7bf210dec930e
Drew Hamilton <github@drewhamilton.dev>
2023-09-13 16:59:16 -0700

    Update displayed time on resume

--- demo/src/main/java/dev/drewhamilton/androidtime/format/demo/DemoActivity.kt
+++ demo/src/main/java/dev/drewhamilton/androidtime/format/demo/DemoActivity.kt
@@ -5,21 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import dev.drewhamilton.androidtime.format.demo.ui.theme.DemoTheme
 import java.time.Instant
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class DemoActivity : ComponentActivity() {
+
+    private val latestInstant = MutableStateFlow(Instant.now())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             DemoTheme {
+                val instant by latestInstant.collectAsState()
                 Demo(
-                    instant = Instant.now(),
+                    instant = instant,
                     modifier = Modifier.fillMaxSize(),
                 )
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        latestInstant.value = Instant.now()
     }
 }
