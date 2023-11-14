34b071a94ebe772446ef78c640d48e39a240f31d
Drew Hamilton <github@drewhamilton.dev>
2023-11-14 14:53:40 -0800

    Update Persian MMMMd skeleton result for all API levels

--- datetimeformatters/src/androidTest/java/dev/drewhamilton/androidtime/format/AndroidDateTimeFormatterTest.kt
+++ datetimeformatters/src/androidTest/java/dev/drewhamilton/androidtime/format/AndroidDateTimeFormatterTest.kt
@@ -937,10 +937,7 @@ class AndroidDateTimeFormatterTest(
             mediumDate = "7 سپتامبر 2023",
             longDate = "7 سپتامبر 2023",
             fullDate = "پنجشنبه 7 سپتامبر 2023",
-            skeletonMMMMd = when {
-                Build.VERSION.SDK_INT >= 28 -> "7 اکتبر"
-                else -> "7 سپتامبر"
-            }
+            skeletonMMMMd = "7 سپتامبر"
         ),
         ;
 
