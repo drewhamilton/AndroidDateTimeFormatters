bcb48642eb577f7c043437c4b9bcc6cc0832b836
Drew Hamilton <github@drewhamilton.dev>
2026-01-18 14:07:30 -0600

    Update demo app for new minSdk

--- demo/src/main/java/dev/drewhamilton/androidtime/format/demo/contextLocale.kt
+++ demo/src/main/java/dev/drewhamilton/androidtime/format/demo/contextLocale.kt
@@ -1,17 +1,14 @@
 package dev.drewhamilton.androidtime.format.demo
 
 import android.content.Context
-import android.os.Build
 import java.util.Locale
 
 internal fun Context.extractPrimaryLocale(): Locale {
     val configuration = resources.configuration
     var locale: Locale? = null
-    if (Build.VERSION.SDK_INT >= 24) {
-        val localeList = configuration.locales
-        if (!localeList.isEmpty) {
-            locale = localeList[0]
-        }
+    val localeList = configuration.locales
+    if (!localeList.isEmpty) {
+        locale = localeList[0]
     }
     if (locale == null) {
         @Suppress("DEPRECATION") // Fallback for API < 24
